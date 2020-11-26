2ff2c814c399c1e2bb193a66de4451e0bff53457
Ashish Kumar Verma <ashish1500616@gmail.com>
2020-11-27 00:12:31 +0530

    Inital Commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Expense App'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Card(
+              child: Text("Chart"),
+            ),
+            Card(
+              child: Text("List Of Txn"),
+            )
+          ],
+        ));
+  }
+}
