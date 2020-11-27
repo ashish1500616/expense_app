ead0c78a4d08a3f412bbf57cc09c2c730c2fcc30
Ashish Kumar Verma <ashish1500616@gmail.com>
2020-11-27 23:16:22 +0530

    Use List View To Make Content Scrollable.

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,48 +10,49 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-        children: transactions.map((tx) {
-      return Card(
-        child: Row(
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.all(10),
-              margin: EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.black,
-                  width: 2,
-                ),
-              ),
-              child: Text(
-                '\$ ${tx.amount}',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  color: Colors.blue,
-                ),
-              ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  tx.title,
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
+      children: transactions.map((tx) {
+        return Card(
+          child: Row(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 2,
                   ),
                 ),
-                Text(
-                  // DateFormat('yyyy-MM-dd').format(tx.date),
-                  DateFormat.yMd().format(tx.date),
-                  style: TextStyle(color: Colors.grey),
+                child: Text(
+                  '\$ ${tx.amount}',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    color: Colors.blue,
+                  ),
                 ),
-              ],
-            )
-          ],
-        ),
-      );
-    }).toList());
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    tx.title,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    // DateFormat('yyyy-MM-dd').format(tx.date),
+                    DateFormat.yMd().format(tx.date),
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                ],
+              )
+            ],
+          ),
+        );
+      }).toList(),
+    );
   }
 }
